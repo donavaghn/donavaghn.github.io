389e41ea3c1e48196e79d65d71a81c1d04603826
Donavaghn <DonavaghnJarvie@gmail.com>
2018-06-18 16:52:16 -0600

    removed lazy image

--- cit230/lazyimage/lazyimg.js
+++
@@ -1,8 +0,0 @@
-var image = document.getElementById("lazy")
-var bigimage = document.createElement("img")
-
-bigimage.onload = function() {
-	image.src = this.src
-}
-
-setTimeout(function() {bigimage.src = "images/amsterdam-architecture-boat-large.jpg"}, 5)
