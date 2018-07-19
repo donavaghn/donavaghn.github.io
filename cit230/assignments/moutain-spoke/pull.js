6c40dca3cfe2a16e960fbb5f95e9274c834323d8
Donavaghn <DonavaghnJarvie@gmail.com>
2018-07-18 21:38:05 -0600

    trying

--- cit230/assignments/moutain-spoke/pull.js
+++ cit230/assignments/moutain-spoke/pull.js
@@ -1,4 +1,4 @@
-    var table = document.querySelector('table');
+var table = document.querySelector('table');
 
 var requestURL = 'services.json';
 
@@ -10,51 +10,26 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-  var dataForTable = request.response;
-  showTable(dataForTable);
+   var servicesInfo = request.response;
+   showTable(servicesInfo);
 }
 
 function showTable(jsonObj) {
-  var services = jsonObj['services'];
+   var serve = jsonObj['services'];
      
-  for (var i = 0; i < services.length; i++) {
+   for (var i = 0; i < services.length; i++) {
 	 var row = document.createElement('tr');
     var name = document.createElement('td');
 	 var price = document.createElement('td');
-	 name.textContent = services[i].name
-	 price.textContent = services[i].price
+	 name.textContent = serve[i].name;
+	 price.textContent = serve[i].price;
 	  
 	 row.appendChild(name);
 	 row.appendChild(price);
 	 table.appendChild(tr);
-	 
-	 
-	  
-//	  var myArticle = document.createElement('article');
-//    var myH2 = document.createElement('h2');
-//    var myPara1 = document.createElement('p');
-//    var myPara2 = document.createElement('p');
-//    var myPara3 = document.createElement('p');
-//    var myList = document.createElement('ul');
-//
-//    myH2.textContent = heroes[i].name;
-//    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-//    myPara2.textContent = 'Age: ' + heroes[i].age;
-//    myPara3.textContent = 'Superpowers:';
-//        
-//    var superPowers = heroes[i].powers;
-//    for (var j = 0; j < superPowers.length; j++) {
-//      var listItem = document.createElement('li');
-//      listItem.textContent = superPowers[j];
-//      myList.appendChild(listItem);
     }
-	  
-//    myArticle.appendChild(myH2);
-//    myArticle.appendChild(myPara1);
-//    myArticle.appendChild(myPara2);
-//    myArticle.appendChild(myPara3);
-//    myArticle.appendChild(myList);
-//	  
-//	 section.appendChild(myArticle);
+
   }
-//}
+
+
+
